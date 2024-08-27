9ecdcf56cf735083d0ce40a67ca22cda1d7e75e5
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-08-27 13:26:09 +0100

    feat: lauch hoopla usage graphs

---
+++ code/web/services/Hoopla/UsageGraphs.php
@@ -0,0 +1,31 @@
+<?php
+require_once ROOT_DIR . '/services/Admin/AbstractUsageGraphs.php';
+require_once ROOT_DIR . '/sys/Hoopla/UserHooplaUsage.php';
+require_once ROOT_DIR . '/sys/Hoopla/HooplaRecordUsage.php';
+require_once ROOT_DIR . '/sys/Utils/GraphingUtils.php';
+
+class Hoopla_UsageGraphs extends Admin_AbstractUsageGraphs {
+	function launch(): void {
+		$this->launchGraph('Hoopla');
+	}
+
+	function getBreadcrumbs(): array {
+		$breadcrumbs = [];
+		$breadcrumbs[] = new Breadcrumb('/Admin/Home', 'Administration Home');
+		$breadcrumbs[] = new Breadcrumb('/Admin/Home#hoopla', 'Hoopla');
+		$breadcrumbs[] = new Breadcrumb('/Hoopla/Dashboard', 'Usage Dashboard');
+		$breadcrumbs[] = new Breadcrumb('', 'Usage Graphs');
+		return $breadcrumbs;
+	}
+
+	function getActiveAdminSection(): string {
+		return 'hoopla';
+	}
+
+	function canView(): bool {
+		return UserAccount::userHasPermission([
+			'View System Reports',
+			'View Dashboards',
+		]);
+	}
+}
