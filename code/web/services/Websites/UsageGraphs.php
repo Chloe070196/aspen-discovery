7d50a3cf82ba2c5da02ceddf20a815f670d85d10
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-08-27 12:42:15 +0100

    DIS-102: feat: export website usage data to csv
    fetches fresh usage data, generates a CSV file, and triggers its
    download

--- code/web/services/Websites/UsageGraphs.php
+++ code/web/services/Websites/UsageGraphs.php
@@ -22,6 +22,13 @@ class Websites_UsageGraphs extends Admin_Admin {
 
 		$websiteIndexSettingId = $this->getWebsiteIndexSettingIdBy($websiteName);
 		$this->getAndSetInterfaceDataSeries($stat, $instanceName, $websiteIndexSettingId);
+		$interface->assign('websiteName', $websiteName);
+
+		$interface->assign('stat', $stat);
+		$interface->assign('propName', 'exportToCSV');
+		$interface->assign('showCSVExportButton', true);
+		$interface->assign('section', 'Websites');
+		
 		$this->display('../Admin/usage-graph.tpl', $title);
 	}
 
@@ -43,6 +50,53 @@ class Websites_UsageGraphs extends Admin_Admin {
 			'View System Reports',
 			'View Dashboards',
 		]);
+	}
+
+	// note that this will only handle tables with one stat (as is needed for websites usage data)
+	// to see a version that handle multpile stats, see the Admin/UsageGraphs.php implementation
+	public function buildCSV() {
+		global $interface;
+		$stat = $_REQUEST['stat'];
+		if (!empty($_REQUEST['instance'])) {
+			$instanceName = $_REQUEST['instance'];
+		} else {
+			$instanceName = '';
+		}
+
+		$websiteName= $_REQUEST['websiteName'];
+		$websiteIndexSettingId = $this->getWebsiteIndexSettingIdBy($websiteName);
+		$this->getAndSetInterfaceDataSeries($stat, $instanceName, $websiteIndexSettingId);
+		$dataSeries = $interface->getVariable('dataSeries');
+
+		$filename = "WebsitesUsageData_{$stat}.csv";
+		header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+		header("Cache-Control: no-store, no-cache, must-revalidate");
+		header("Cache-Control: post-check=0, pre-check=0", false);
+		header("Pragma: no-cache");
+		header('Content-Type: text/csv; charset=utf-8');
+		header("Content-Disposition: attachment;filename={$filename}");
+		$fp = fopen('php://output', 'w');
+
+		// builds the first row of the table in the CSV - column headers: Dates, and the title of the graph
+		fputcsv($fp, ['Dates', $stat]);
+
+		// builds each subsequent data row - aka the column value
+		foreach ($dataSeries as $dataSerie) {
+			$data = $dataSerie['data'];
+			$numRows = count($data);
+			$dates = array_keys($data);
+
+			if( empty($numRows)) {
+				fputcsv($fp, ['no data found!']);
+			}
+			for($i = 0; $i < $numRows; $i++) {
+				$date = $dates[$i];
+				$value = $data[$date];
+				$row = [$date, $value];
+				fputcsv($fp, $row);
+			}
+		}
+		exit();
 	}
 
 	/*
@@ -130,7 +184,6 @@ class Websites_UsageGraphs extends Admin_Admin {
 				}
 			}
 		}
-
 
 		$interface->assign('columnLabels', $columnLabels);
 		$interface->assign('dataSeries', $dataSeries);
