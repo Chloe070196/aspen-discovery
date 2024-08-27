e1427c94087d1972b1e9d7af80e57890d17bfd38
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-08-27 13:31:49 +0100

    feat: display hoopla usage data in graph
    fetch fresh data for the selected stat and display it in a graph and a
    raw data table

--- code/web/services/Hoopla/UsageGraphs.php
+++ code/web/services/Hoopla/UsageGraphs.php
@@ -29,6 +29,78 @@ class Hoopla_UsageGraphs extends Admin_AbstractUsageGraphs {
 		]);
 	}
 
+	protected function getAndSetInterfaceDataSeries($stat, $instanceName): void {
+		global $interface;
+
+		$dataSeries = [];
+		$columnLabels = [];
+
+		// for the graph displaying data retrieved from the user_website_usage table
+		if ($stat == 'activeUsers') {
+			$userUsage = new UserHooplaUsage();
+			$userUsage->groupBy('year, month');
+			if (!empty($instanceName)) {
+				$userUsage->instance = $instanceName;
+			}
+			$userUsage->selectAdd();
+			$userUsage->selectAdd('year');
+			$userUsage->selectAdd('month');
+			$userUsage->orderBy('year, month');
+
+			$dataSeries['Active Users'] = GraphingUtils::getDataSeriesArray(count($dataSeries));
+			$userUsage->selectAdd('COUNT(id) as numUsers');
+
+			$userUsage->find();
+			while ($userUsage->fetch()) {
+				$curPeriod = "{$userUsage->month}-{$userUsage->year}";
+				$columnLabels[] = $curPeriod;
+				/** @noinspection PhpUndefinedFieldInspection */
+				$dataSeries['Active Users']['data'][$curPeriod] = $userUsage->numUsers;
+			}
+		}
+
+		// for the graph displaying data retrieved from the website_page_usage table
+		if ($stat == 'recordsUsed' || $stat == 'loans') {
+			$usage = new HooplaRecordUsage();
+			$usage->groupBy('year, month');
+			if (!empty($instanceName)) {
+				$usage->instance = $instanceName;
+			}
+
+			$usage->selectAdd();
+			$usage->selectAdd('year');
+			$usage->selectAdd('month');
+			$usage->orderBy('year, month');
+			if ($stat == 'recordsUsed') {
+				$dataSeries['Records With Usage'] = GraphingUtils::getDataSeriesArray(count($dataSeries));
+				$usage->selectAdd('COUNT(id) as numRecordsUsed');
+			}
+			if ($stat == 'loans') {
+				$dataSeries['Loans'] = GraphingUtils::getDataSeriesArray(count($dataSeries));
+				$usage->selectAdd('SUM(timesCheckedOut) as totalCheckouts');
+			}
+
+			$usage->find();
+			while ($usage->fetch()) {
+				$curPeriod = "{$usage->month}-{$usage->year}";
+				$columnLabels[] = $curPeriod;
+				if ($stat == 'recordsUsed') {
+					/** @noinspection PhpUndefinedFieldInspection */
+					$dataSeries['Records With Usage']['data'][$curPeriod] = $usage->numRecordsUsed;
+				}
+				if ($stat == 'loans') {
+					/** @noinspection PhpUndefinedFieldInspection */
+					$dataSeries['Loans']['data'][$curPeriod] = $usage->totalCheckouts;
+				}
+			}
+		}
+
+		$interface->assign('columnLabels', $columnLabels);
+		$interface->assign('dataSeries', $dataSeries);
+		$interface->assign('translateDataSeries', true);
+		$interface->assign('translateColumnLabels', false);
+	}
+
 	private function assignGraphSpecificTitle($stat) {
 		global $interface;
 		$title = $interface->getVariable('graphTitle');
