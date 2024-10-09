a74eb1b93485402e8a80bcea8bba58fcab0e86c1
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-10-09 16:48:27 +0100

    feat: add Resource Sharing For Groups Permissions
    Creates the two following permissions in the
    Interlibrary Loans section in Aspen Administration
    > System Administration > Permissions:
    - Administer OCLC Resource Sharing For Groups
    Settings
    - Administer OCLC Resource Sharing For Groups
    Forms
    
    Test plan:
    - run the DB maintenance update
    - ensure that the OCLC Resource Sharing For Groups
    is enabled in Aspen Administration > Systems
    Administration > Modules
    - navigate to Aspen Administration
    > System Administration > Permissions
    > Interlibrary Loans
    - notice the two new permissions

--- code/web/sys/DBMaintenance/oclcResourceSharingForGroups_updates.php
+++ code/web/sys/DBMaintenance/oclcResourceSharingForGroups_updates.php
@@ -11,5 +11,15 @@ function getOclcResourceSharingForGroupsUpdates()
                 "INSERT INTO modules (name) VALUES ('OCLC Resource Sharing For Groups')",
             ],
         ],
+        'create_oclc_resource_sharing_for_groups_permissions' => [
+            'title' => 'Create OCLC Resource Sharing For Groups Permissions',
+            'description' => 'Add an OCLC Resource Sharing For Groups permission section containing the permissions to do with this module',
+            'sql' => [
+                "INSERT INTO permissions (name, sectionName, requiredModule, weight, description) VALUES ( 'Administer OCLC Resource Sharing For Groups Settings','ILL Integration','OCLC Resource Sharing For Groups', 0, 'Allows the user to administer the integration with OCLC Resource Sharing For Groups')",
+                "INSERT INTO permissions (name, sectionName, requiredModule, weight, description) VALUES ( 'Administer OCLC Resource Sharing For Groups Forms','ILL Integration','OCLC Resource Sharing For Groups', 0, 'Allows the user to administer the OCLC Resource Sharing For Groups ILL request forms')",
+            ],
+            // TODO:consider an 'administer forms for their library' permission
+            // FIXME: edit weighting so permissions interact proerly with one another
+        ],
     ];
 }
