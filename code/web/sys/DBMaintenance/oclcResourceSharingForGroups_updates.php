c2b9dc6a377de0388223ad0b328df8b7b79c8c70
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-10-24 17:15:36 +0100

    chore: remove whitespace

--- code/web/sys/DBMaintenance/oclcResourceSharingForGroups_updates.php
+++ code/web/sys/DBMaintenance/oclcResourceSharingForGroups_updates.php
@@ -3,23 +3,23 @@
 
 function getOclcResourceSharingForGroupsUpdates()
 {
-    return [
-        'create_oclc_resource_sharing_for_groups_module' => [
-            'title' => 'Create OCLC Resource Sharing For Groups Module',
-            'description' => 'Add OCLC Resource Sharing For Groups to the list of modules',
-            'sql' => [
-                "INSERT INTO modules (name) VALUES ('OCLC Resource Sharing For Groups')",
-            ],
-        ],
-        'create_oclc_resource_sharing_for_groups_permissions' => [
-            'title' => 'Create OCLC Resource Sharing For Groups Permissions',
-            'description' => 'Add an OCLC Resource Sharing For Groups permission section containing the permissions to do with this module',
-            'sql' => [
-                "INSERT INTO permissions (name, sectionName, requiredModule, weight, description) VALUES ( 'Administer OCLC Resource Sharing For Groups Settings','ILL Integration','OCLC Resource Sharing For Groups', 0, 'Allows the user to administer the integration with OCLC Resource Sharing For Groups')",
-                "INSERT INTO permissions (name, sectionName, requiredModule, weight, description) VALUES ( 'Administer OCLC Resource Sharing For Groups Forms','ILL Integration','OCLC Resource Sharing For Groups', 0, 'Allows the user to administer the OCLC Resource Sharing For Groups ILL request forms')",
-            ],
-            // TODO:consider an 'administer forms for their library' permission
-        ],
+	return [
+		'create_oclc_resource_sharing_for_groups_module' => [
+			'title' => 'Create OCLC Resource Sharing For Groups Module',
+			'description' => 'Add OCLC Resource Sharing For Groups to the list of modules',
+			'sql' => [
+				"INSERT INTO modules (name) VALUES ('OCLC Resource Sharing For Groups')",
+			],
+		],
+		'create_oclc_resource_sharing_for_groups_permissions' => [
+			'title' => 'Create OCLC Resource Sharing For Groups Permissions',
+			'description' => 'Add an OCLC Resource Sharing For Groups permission section containing the permissions to do with this module',
+			'sql' => [
+				"INSERT INTO permissions (name, sectionName, requiredModule, weight, description) VALUES ( 'Administer OCLC Resource Sharing For Groups Settings','ILL Integration','OCLC Resource Sharing For Groups', 0, 'Allows the user to administer the integration with OCLC Resource Sharing For Groups')",
+				"INSERT INTO permissions (name, sectionName, requiredModule, weight, description) VALUES ( 'Administer OCLC Resource Sharing For Groups Forms','ILL Integration','OCLC Resource Sharing For Groups', 0, 'Allows the user to administer the OCLC Resource Sharing For Groups ILL request forms')",
+			],
+			// TODO:consider an 'administer forms for their library' permission
+		],
 		'create_oclc_resource_sharing_for_groups_form_table' => [
 			'title' => 'Add the OCLC Resource Sharing For Groups Form Table',
 			'description' => 'Add a table to store the forms created by admins to keep track of which optional fields should be displayed',
@@ -104,5 +104,5 @@ function getOclcResourceSharingForGroupsUpdates()
 			],
 		],
 
-    ];
+	];
 }
