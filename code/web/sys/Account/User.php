42afc994b0c872e78d909f1e7b0c8d5a16424fc8
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-11-20 11:52:09 +0000

    feat: display num of ILL requests

--- code/web/sys/Account/User.php
+++ code/web/sys/Account/User.php
@@ -1858,9 +1858,6 @@ class User extends DataObject {
 					$allHolds = array_merge_recursive($allHolds, $vdxRequests);
 					$holdsToReturn = array_merge_recursive($holdsToReturn, $vdxRequests);
 				}
-			}
-
-			if ($source == 'all' || $source == 'interlibrary_loan') {
 				if ($this->hasOCLCResourceSharingForGroupsInterlibraryLoan()) {
 					require_once ROOT_DIR . '/Drivers/OCLCResourceSharingForGroupsDriver.php';
 					require_once ROOT_DIR . '/sys/OCLCResourceSharingForGroups/OCLCResourceSharingForGroupsSetting.php';
@@ -2043,9 +2040,9 @@ class User extends DataObject {
 			uasort($holdsToReturn['unavailable'], $holdSort);
 		}
 
-		if ($source == 'interlibrary_loan') {
-			unset($holdsToReturn['available']);
-		}
+		// if ($source == 'interlibrary_loan') {
+		// 	unset($holdsToReturn['available']);
+		// }
 
 		return $holdsToReturn;
 	}
