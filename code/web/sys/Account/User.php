090c387b23438022a5014c0c6afad5a1e1fa66a6
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-12-12 16:55:01 +0000

    DIS-65 feat: Administer Resource Sharing For Groups Permissions

--- code/web/sys/Account/User.php
+++ code/web/sys/Account/User.php
@@ -4454,6 +4454,13 @@ class User extends DataObject {
 			$sections['aspen_lida']->addAction(new AdminAction('Self-Check Settings', 'Define settings for self-check in Aspen LiDA.', '/AspenLiDA/SelfCheckSettings'), 'Administer Aspen LiDA Self-Check Settings');
 		}
 
+		if (array_key_exists('OCLC Resource Sharing For Groups',  $enabledModules)) {
+			$sections['ill_integration']->addAction(new AdminAction('Resource Sharing For Groups Settings', 'Manage connections to OCLC Resource Sharing For Groups for various profiles', '/OCLCResourceSharingForGroups/OCLCResourceSharingForGroupsSettings'), 'Administer OCLC Resource Sharing For Groups Settings');
+			$sections['ill_integration']->addAction(new AdminAction('Resource Sharing For Groups Forms', 'Create forms for patrons to use to submit an ILL by selecting the fields to be displayed', '/OCLCResourceSharingForGroups/OCLCResourceSharingForGroupsForms'), 'Administer OCLC Resource Sharing For Groups Forms');
+			$sections['ill_integration']->addAction(new AdminAction('Resource Sharing For Groups Hold Groups', 'Administer Hold Groups for use with the OCLC Resource Sharing For Groups integration', '/OCLCResourceSharingForGroups/OCLCResourceSharingForGroupsHoldGroups'), 'Administer OCLC Resource Sharing For Groups Hold Groups');
+			// TODO: add dashboard if / when usage starts being monitored
+		}
+
 		$sections['support'] = new AdminSection('Aspen Discovery Support');
 		$sections['support']->addAction(new AdminAction('Request Tracker Settings', 'Define settings for a Request Tracker support system.', '/Support/RequestTrackerConnections'), 'Administer Request Tracker Connection');
 		try {
