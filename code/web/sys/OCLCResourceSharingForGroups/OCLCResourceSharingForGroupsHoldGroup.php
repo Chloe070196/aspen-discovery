413826c7bf8dfe435f821da51b6dcb122b30cedf
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-12-12 17:07:29 +0000

    DIS-65 feat: Administer RS4G Hold Groups

---
+++ code/web/sys/OCLCResourceSharingForGroups/OCLCResourceSharingForGroupsHoldGroup.php
@@ -0,0 +1,152 @@
+<?php
+
+require_once ROOT_DIR . '/sys/OCLCResourceSharingForGroups/OCLCResourceSharingForGroupsHoldGroupLocation.php';
+
+class OCLCResourceSharingForGroupsHoldGroup extends DataObject {
+	public $__table = 'oclc_resource_sharing_for_groups_hold_group';
+	public $id;
+	public $name;
+
+	protected $_locations;
+	protected $_locationCodes;
+
+	public static function getObjectStructure($context = ''): array {
+		$locationList = Location::getLocationList(false);
+
+		return [
+			'id' => [
+				'property' => 'id',
+				'type' => 'label',
+				'label' => 'Id',
+				'description' => 'The unique id',
+			],
+			'name' => [
+				'property' => 'name',
+				'type' => 'text',
+				'label' => 'Name',
+				'description' => 'The Name of the Hold Group',
+				'maxLength' => 50,
+			],
+
+			'locations' => [
+				'property' => 'locations',
+				'type' => 'multiSelect',
+				'listStyle' => 'checkboxSimple',
+				'label' => 'Locations',
+				'description' => 'Define locations that make up this hold group',
+				'values' => $locationList,
+				'hideInLists' => false,
+			],
+		];
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public function getUniquenessFields(): array {
+		return ['name'];
+	}
+
+	/**
+	 * Override the update functionality to save related objects
+	 *
+	 * @see DB/DB_DataObject::update()
+	 */
+	public function update($context = '') {
+		$ret = parent::update();
+		if ($ret !== FALSE) {
+			$this->saveLocations();
+		}
+		return $ret;
+	}
+
+	public function insert($context = '') {
+		$ret = parent::insert();
+		if ($ret !== FALSE) {
+			$this->saveLocations();
+		}
+		return $ret;
+	}
+
+	public function delete($useWhere = false) : int {
+		$ret = parent::delete($useWhere);
+		if ($ret && !empty($this->id)) {
+			$holdGroupLocation = new OCLCResourceSharingForGroupsHoldGroupLocation();
+			$holdGroupLocation->vdxHoldGroupId = $this->id;
+			$holdGroupLocation->delete(true);
+		}
+		return $ret;
+	}
+
+	public function __get($name) {
+		if ($name == "locations") {
+			return $this->getLocations();
+		} else {
+			return parent::__get($name);
+		}
+	}
+
+	/**
+	 * @return int[]
+	 */
+	public function getLocations(): ?array {
+		if (!isset($this->_locations) && $this->id) {
+			$this->_locations = [];
+			$obj = new OCLCResourceSharingForGroupsHoldGroupLocation();
+			$obj->vdxHoldGroupId = $this->id;
+			$obj->find();
+			while ($obj->fetch()) {
+				$this->_locations[$obj->locationId] = $obj->locationId;
+			}
+		}
+		return $this->_locations;
+	}
+
+	public function getLocationCodes() {
+		if (!isset($this->_locationCodes) && $this->id) {
+			$this->_locationCodes = [];
+			$locationIds = $this->getLocations();
+			foreach ($locationIds as $locationId) {
+				$location = new Location();
+				$location->locationId = $locationId;
+				if ($location->find(true)) {
+					$this->_locationCodes[] = $location->code;
+				}
+			}
+		}
+		return $this->_locationCodes;
+	}
+
+	public function __set($name, $value) {
+		if ($name == "locations") {
+			$this->_locations = $value;
+		} else {
+			parent::__set($name, $value);
+		}
+	}
+
+
+	public function saveLocations() {
+		if (isset ($this->_locations) && is_array($this->_locations)) {
+			$locationList = Location::getLocationList(!UserAccount::userHasPermission('Administer OCLC Resource Sharing For Groups Hold Groups'));
+			foreach ($locationList as $locationId => $displayName) {
+				$obj = new OCLCResourceSharingForGroupsHoldGroupLocation();
+				$obj->vdxHoldGroupId = $this->id;
+				$obj->locationId = $locationId;
+				if (in_array($locationId, $this->_locations)) {
+					if (!$obj->find(true)) {
+						$obj->insert();
+					}
+				} else {
+					if ($obj->find(true)) {
+						$obj->delete();
+					}
+				}
+			}
+		}
+	}
+
+	public function okToExport(array $selectedFilters): bool {
+		return parent::okToExport($selectedFilters);
+	}
+}
