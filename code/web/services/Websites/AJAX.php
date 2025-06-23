cbbb0cf6a923a16b4827ee5322ba8438c501e44b
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2025-06-23 13:32:24 +0100

    DIS-102: style: fix indent

--- code/web/services/Websites/AJAX.php
+++ code/web/services/Websites/AJAX.php
@@ -1,7 +1,7 @@
 <?php
 require_once ROOT_DIR . '/JSON_Action.php';
 class Websites_AJAX extends JSON_Action {
-public function exportUsageData() {
+	public function exportUsageData() {
 		require_once ROOT_DIR . '/services/Websites/UsageGraphs.php';
 		$WebsitesUsageGraph = new Websites_UsageGraphs(); 
 		$WebsitesUsageGraph->buildCSV();
