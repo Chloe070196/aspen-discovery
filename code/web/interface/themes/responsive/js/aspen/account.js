6ec7e28c9d243a2c819a6f8bef0b73165640fb25
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-10-20 13:21:22 +0100

    DIS-1191: feat: allow grouping of linked user holds

--- code/web/interface/themes/responsive/js/aspen/account.js
+++ code/web/interface/themes/responsive/js/aspen/account.js
@@ -3046,6 +3046,7 @@ AspenDiscovery.Account = (function () {
 		},
 		groupSelectedPendingHolds:  function (source, availableSort, interlibrarySort, unavailableSort) {
 			let selectedHolds = [];
+			let userIds = [];
 
 			$("input[name*='select']:checked").each(function() {
 				const nameAttr = $(this).attr('name');
@@ -3053,8 +3054,14 @@ AspenDiscovery.Account = (function () {
 					const idMatches = nameAttr.match(/\d+/g);
 					if (idMatches && idMatches.length >= 3) {
 						const holdId = idMatches[2];
+						const userId = idMatches[0];
+
 						if (holdId && !selectedHolds.includes(holdId)) {
 							selectedHolds.push(holdId);
+						}
+
+						if (userId && !userIds.includes(userId)) {
+							userIds.push(userId);
 						}
 					}
 				}
@@ -3066,7 +3073,8 @@ AspenDiscovery.Account = (function () {
 				holdIds: selectedHolds,
 				availableSort: availableSort,
 				interlibrarySort: interlibrarySort,
-				unavailableSort: unavailableSort
+				unavailableSort: unavailableSort,
+				userIds: userIds
 			};
 
 			$.getJSON(url, params, function(data) {
