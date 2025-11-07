37b971fbdd90f96325c0df02862af5f0211b287a
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-11-07 16:21:13 +0000

    DIS-1565: feat: update db

--- code/web/sys/DBMaintenance/version_updates/26.Q1.00.php
+++ code/web/sys/DBMaintenance/version_updates/26.Q1.00.php
@@ -33,5 +33,23 @@ function getUpdates26_Q1_00(): array {
 				"ALTER TABLE library ADD COLUMN workPhoneField CHAR(1) DEFAULT 'p'"
 			]
 		], //sierra_phone_fields
+		//alexander - Open Fifth
+		 'change_data_types_for_grapes_js_columns' => [
+			'title' => 'Change Data Types For Grapes JS Columns',
+			'description' => 'Update column types to allow for longer pages',
+			'continueOnError' => false,
+			'sql' => [
+				"ALTER TABLE grapes_web_builder MODIFY templateContent LONGTEXT",
+				"ALTER TABLE grapes_web_builder MODIFY htmlData LONGTEXT",
+				"ALTER TABLE grapes_web_builder MODIFY cssData LONGTEXT",
+			]
+		], //change_data_types_for_grapes_js_columns
+		'add_option_to_add_location_to_event_thumbail_image' => [
+			'title' => 'Add Option to Add Location to Event Thumnail Image',
+			'description' => 'Add ability to choose to add event location to event thumbnail image',
+			'sql' => [
+				"ALTER TABLE event ADD COLUMN displayEventBranchOnThumbnail TINYINT(1) DEFAULT 0"
+			]
+		], //add_option_to_add_location_to_event_thumbnail_image 
 	];
 }
