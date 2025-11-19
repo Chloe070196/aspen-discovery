69cd762dd3aac5710843b07167fd9fa9c2d5ba58
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2025-11-19 15:23:42 +0000

    rebuild apsen.js

--- code/web/interface/themes/responsive/js/aspen.js
+++ code/web/interface/themes/responsive/js/aspen.js
@@ -10677,9 +10677,20 @@ AspenDiscovery.Events = (function(){
 			}else{
 				AspenDiscovery.Events.saveEventsObjCallback();
 			}
+		},
+
+		lessEventDates: function() {
+			document.getElementById("moreEventDates").style.display = "block";
+			document.getElementById("narrowGroupHidden_eventDates").style.display = "none";
+		},
+
+		moreEventDates: function() {
+			document.getElementById("moreEventDates").style.display = "none";
+			document.getElementById("narrowGroupHidden_eventDates").style.display = "block";
 		}
 	};
 }(AspenDiscovery.Events || {}));
+
 AspenDiscovery.GroupedWork = (function(){
 	return {
 		hasTableOfContentsInRecord: false,
