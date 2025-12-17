27ad600dc98edfd1367c0ccf97a3451cc71a87df
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-12-17 15:19:35 +0000

    DIS-1715: feat: add waitingListNumberOfSeats
    Add waitingListNumberOfSeats to event and event_instance

--- code/web/sys/DBMaintenance/aspen_event_registration_updates.php
+++ code/web/sys/DBMaintenance/aspen_event_registration_updates.php
@@ -50,5 +50,13 @@ function getAspenEventRegistrationUpdates() {
 				'ALTER TABLE event_instance ADD COLUMN waitingList TINYINT(1) DEFAULT NULL',
 			],
 		], // add_waitingList_to_events_and_instances
+		'add_waitingListNumberOfSeats_to_events' => [
+			'title' => 'Add waitingListNumberOfSeats to Events',
+			'description' => 'Add waiting list number of seats column to Events and Event Instances',
+			'sql' => [
+				'ALTER TABLE event ADD COLUMN waitingListNumberOfSeats TINYINT(1) DEFAULT NULL',
+				'ALTER TABLE event_instance ADD COLUMN waitingListNumberOfSeats TINYINT(1) DEFAULT NULL',
+			],
+		], // add_waitingListNumberOfSeats_to_events
 	];
 }
