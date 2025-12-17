9d732d81bf37d04cba9ea55272070e7f3aab5868
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2025-12-17 15:13:56 +0000

    DIS-1068: chore: remove logger

--- code/web/scripts/pay360-poll.php
+++ code/web/scripts/pay360-poll.php
@@ -24,9 +24,6 @@ $timer = new Timer();
 global $configArray;
 $configArray = readConfig();
 
-global $logger;
-$logger = new Logger();
-
 initDatabase();
 
 $pay360SettingsId = intval($argv[2]) ?? null;
