1e8452046117b716a632d71a2c52bf97027909cc
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2026-01-28 14:25:06 +0000

    DIS-1597: feat: cap number of seats to 1000

--- code/web/sys/Events/EventInstance.php
+++ code/web/sys/Events/EventInstance.php
@@ -78,6 +78,7 @@ class EventInstance extends DataObject {
 				'label' => 'Number of Seats Override',
 				'description' => 'Override capacity for this specific instance. Leave blank to use event default.',
 				'min' => 0,
+				'max' => 1000,
 			],
 			'waitingList' => [
 				'property' => 'waitingList',
