c2317b44d49b07cf9d294e7334bd749fa37ebe75
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2026-03-10 07:39:16 +0000

    WIP: DIS-1909: update waiting list positions

--- code/web/sys/Events/UserAspenEventInstanceWaitingList.php
+++ code/web/sys/Events/UserAspenEventInstanceWaitingList.php
@@ -14,4 +14,52 @@ class UserAspenEventInstanceWaitingList extends DataObject {
 	public $canRegister;
 	public $canRegisterUntil;
 	public $toastShown;
+
+	public function updateWaitingListPositions(): void {
+		require_once ROOT_DIR . '/sys/Events/Event.php';
+		$event = new Event();
+		$event->id = $eventInstance->eventId;
+		if (!$event->find(true)) {
+			return;
+		}
+		if ((int)$event->waitingListNumberOfSeats <= (int)$eventInstance->availableNumberOfWaitingListSeats) {
+			return;
+		}
+		require_once ROOT_DIR . '/sys/Events/EventInstance.php';
+		$eventInstance = new EventInstance();
+		$eventInstance->id = $eventInstanceId;
+		if (!$eventInstance->find(true)) {
+			return;
+		}
+
+		$this->eventInstanceId = $eventInstanceId;
+		$currentPosition = $this->position;
+		$this->find();
+		while ($listEntry = $this->fetch()) {
+			(int)$listEntry->position--;
+			$listEntry->update();
+			$currentPosition++;
+		}
+
+	}
+	
+	public function incrementAvailableWaitingListSeats($eventInstanceId): void {
+		require_once ROOT_DIR . '/sys/Events/Event.php';
+		$event = new Event();
+		$event->id = $eventInstance->eventId;
+		if (!$event->find(true)) {
+			return;
+		}
+		if ((int)$event->waitingListNumberOfSeats <= (int)$eventInstance->availableNumberOfWaitingListSeats) {
+			return;
+		}
+		require_once ROOT_DIR . '/sys/Events/EventInstance.php';
+		$eventInstance = new EventInstance();
+		$eventInstance->id = $eventInstanceId;
+		if ($eventInstance->find(true)) {
+			$eventInstance->availableNumberOfWaitingListSeats++;
+			$logger->log("updated number of waiting list seats", Logger::LOG_ERROR);
+			$eventInstance->update();
+		}
+	}
 }
